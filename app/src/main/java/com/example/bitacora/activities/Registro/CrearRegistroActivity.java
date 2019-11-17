a06ec9b067bc92148c5777fda3882f3755b3dba7
Castjuanito <juanario1997@gmail.com>
2019-11-17 11:39:34 -0500

    añadido tomar fotos

--- app/src/main/java/com/example/bitacora/activities/Registro/CrearRegistroActivity.java
+++ app/src/main/java/com/example/bitacora/activities/Registro/CrearRegistroActivity.java
@@ -5,32 +5,50 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.bitacora.R;
+import com.example.bitacora.SQLRegistro;
 import com.example.bitacora.activities.Registro.location.GPSTracker;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class CrearRegistroActivity extends AppCompatActivity {
 
+    private static final String FILE_NAME = "example.txt";
+    String currentPhotoPath;
+
+
     private static final int REQUEST_READ_EXTERNAL_STORAGE = 392;
-    private static final int REQUEST_LOCATION = 758;
     private static final int REQUEST_CAMERA = 485;
     private static final int IMAGE_CAPTURE_REQUEST = 615;
     private static final int IMAGE_PICKER_REQUEST = 31;
-    private static final int PLACE_PICKER_REQUEST = 997;
 
     private List<String> imagesName;
     private List<Bitmap> images;
@@ -45,6 +63,7 @@ public class CrearRegistroActivity extends AppCompatActivity {
     private Button aniadirAudio;
     private Button aniadirFoto;
 
+
     double latitude = 0; // latitude
     double longitude = 0; // longitude
 
@@ -52,6 +71,8 @@ public class CrearRegistroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_registro);
+
+        SQLRegistro mSQLAlistamiento = new SQLRegistro(this);
 
         this.tituloText = (EditText) findViewById(R.id.editTextTituloRegistro);
         this.descripcionText = (EditText) findViewById(R.id.editTextDescripcionRegistro);
@@ -62,6 +83,8 @@ public class CrearRegistroActivity extends AppCompatActivity {
         this.aniadirAudio = (Button) findViewById(R.id.buttonAñadirAudioCrearRegistro);
         this.aniadirFoto = (Button) findViewById(R.id.buttonAñadirFotosCrearRegistro);
 
+        imagesName = new ArrayList<String>();
+        images = new ArrayList<>();
 
         /*if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
@@ -73,6 +96,8 @@ public class CrearRegistroActivity extends AppCompatActivity {
             updateCargarImagen();
         }*/
 
+
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
             requestPermission(this, Manifest.permission.CAMERA,
@@ -82,7 +107,21 @@ public class CrearRegistroActivity extends AppCompatActivity {
             updateTomarFoto();
         }
 
+        crearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String titulo = tituloText.getText().toString();
+                String file =  save();
+                addToGallery();
+            }
+        });
 
+        cancelarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                load();
+            }
+        });
 
     }
 
@@ -120,6 +159,24 @@ public class CrearRegistroActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case IMAGE_CAPTURE_REQUEST: {
+                if (resultCode == RESULT_OK) {
+                    File imgFile = new  File(currentPhotoPath);
+                    if(imgFile.exists())            {
+
+                    }
+                }
+                break;
+            }
+
+        }
+    }
+
+
     private void updateCargarImagen() {
         aniadirFoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -135,13 +192,20 @@ public class CrearRegistroActivity extends AppCompatActivity {
         aniadirFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent tomarFoto_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (tomarFoto_intent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(tomarFoto_intent, IMAGE_CAPTURE_REQUEST);
-                }
+                sendTakePictureIntent();
+
             }
         });
     }
+
+    private File getPictureFile() throws IOException {
+            String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+            String pictureFile = "Bitacora_" + timeStamp;
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            File image = File.createTempFile(pictureFile,  ".jpg", storageDir);
+        currentPhotoPath = image.getAbsolutePath();
+            return image;
+        }
 
 
     private void updateGetLocation() {
@@ -153,6 +217,106 @@ public class CrearRegistroActivity extends AppCompatActivity {
             this.longitude = tracker.getLongitude();
         }
     }
+
+    public String save() {
+        String text = descripcionText.getText().toString();
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+
+            descripcionText.getText().clear();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return getFilesDir() + "/" + FILE_NAME;
+    }
+
+    public void load() {
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+
+            descripcionText.getText().clear();
+            Toast.makeText(this, sb.toString(),
+                    Toast.LENGTH_LONG).show();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
+
+    private void addToGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri picUri = Uri.fromFile(f);
+        galleryIntent.setData(picUri);
+        this.sendBroadcast(galleryIntent);
+    }
+
+
+    private void sendTakePictureIntent() {
+
+        Intent tomarFoto_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        tomarFoto_intent.putExtra(MediaStore.EXTRA_FINISH_ON_COMPLETION, true);
+        if (tomarFoto_intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(tomarFoto_intent, IMAGE_CAPTURE_REQUEST);
+            File pictureFile = null;
+            try {
+                pictureFile = getPictureFile();
+            } catch (IOException ex) {
+                Toast.makeText(this,
+                        "Photo file can't be created, please try again",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (pictureFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.bitacora.fileprovider",
+                        pictureFile);
+                tomarFoto_intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(tomarFoto_intent, IMAGE_CAPTURE_REQUEST);
+            }
+        }
+    }
+
 
 
 }
